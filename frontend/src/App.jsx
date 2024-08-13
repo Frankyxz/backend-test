b60f63ceede668a5cfb706f14f0517ece1991bee
Frankyxz <dechavezedcel160@gmail.com>
2024-08-13 13:36:10 +0800

    ggg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -131,7 +131,7 @@ function App() {
         />
         <input
           type="text"
-          value={rfid}
+          value={rfid.join("")}
           onChange={(e) => setRfid(e.target.value)}
         />
 
