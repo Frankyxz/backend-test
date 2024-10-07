f8d3621ba3b65be02deffb24a67e3b306aebc8c5
Frankyxz <dechavezedcel160@gmail.com>
2024-10-07 09:29:09 +0800

    NGROOKO

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -63,11 +63,48 @@ function App() {
       });
   };
 
+  const printReceiptNGROK = () => {
+    const receiptData = `
+          \x1b\x40
+          \x1b\x61\x01
+          \x0a\x0a\x0a
+          ELI IT Solutions POS
+          Paso De Blas Valenzuela
+          City, 1440 Philippines
+          \x0a\x0a\x0a
+          \x1b\x61\x00
+          Item                          Price
+          -----------------------------------
+          Product 1                     2.99
+          Product 2                     1.49
+          Product 3 (12 pack)           3.79
+          Product 4 (1L)                1.29
+          \x0a\x0a\x0a
+          Total:                        9.56
+          Thank you for shopping with us!
+          \x0a\x0a\x0a
+          \x1b\x64\x02
+          \x1d\x56\x01 / Full Cut
+        `;
+
+    axios
+      .post(`https://09d0-112-200-35-24.ngrok-free.app/print`, {
+        data: receiptData,
+      })
+      .then((response) => {
+        console.log("Printed Successfully:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error printing:", error);
+      });
+  };
+
   return (
     <>
       <button onClick={printReceipt}>Print Receipt</button>
       <button onClick={handleTest}>TEST</button>
       <button onClick={handleTestNGROK}>TEST NGROK</button>
+      <button onClick={printReceiptNGROK}>REQUEST TO LOCAL</button>
     </>
   );
 }
