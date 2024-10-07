3f853c7ab6ec9bc5def7bef4ad763c443f22036f
Frankyxz <dechavezedcel160@gmail.com>
2024-10-07 11:43:33 +0800

    ws

--- frontend/src/SocketSample.jsx
+++ frontend/src/SocketSample.jsx
@@ -4,7 +4,7 @@ const SocketSample = () => {
 
   useEffect(() => {
     // Establish WebSocket connection to local WebSocket server
-    const ws = new WebSocket("wss://ee72-112-200-35-24.ngrok-free.app");
+    const ws = new WebSocket("ws://192.168.10.233:8080");
     setSocket(ws);
 
     ws.onopen = () => {
